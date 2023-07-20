b4990cb00ca91b5abbec9399ce2e218177db6b0a
Alexander Raszka <araszka94@gmail.com>
2023-07-20 20:00:31 +0200

    #27 Prevent duplicate main script and force main script to end of file
    - Add missing DuplicateMainManiaScriptException

---
+++ src/ManiaTemplates/Exceptions/DuplicateMainManiaScriptException.cs
@@ -0,0 +1,18 @@
+﻿namespace ManiaTemplates.Exceptions;
+
+public class DuplicateMainManiaScriptException : Exception
+{
+    public DuplicateMainManiaScriptException()
+    {
+    }
+
+    public DuplicateMainManiaScriptException(string message)
+        : base(message)
+    {
+    }
+
+    public DuplicateMainManiaScriptException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
