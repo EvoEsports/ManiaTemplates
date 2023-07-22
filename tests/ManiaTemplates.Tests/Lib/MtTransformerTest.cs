f1125341a4bf25f34e33b86560a117cc07448623
Alexander Raszka <araszka94@gmail.com>
2023-07-22 16:27:22 +0200

    #20 Variables in ManiaScripts
    - Remove debug output

--- tests/ManiaTemplates.Tests/Lib/MtTransformerTest.cs
+++ tests/ManiaTemplates.Tests/Lib/MtTransformerTest.cs
@@ -91,8 +91,6 @@ public class MtTransformerTest
         var result = _transformer.BuildManialink(_testComponent, "expected");
         var generalizedResult = TransformCodeToOrderNumber(result);
         
-        _testOutputHelper.WriteLine(generalizedResult);
-
         Assert.Equal(expected, generalizedResult, ignoreLineEndingDifferences: true);
     }
 
