2d0daa5112840f38df3533e14dff441d02ea68fb
Alexander Raszka <araszka94@gmail.com>
2023-12-07 20:14:15 +0100

    Remove commented out code

--- src/ManiaTemplates/Lib/MtTransformer.cs
+++ src/ManiaTemplates/Lib/MtTransformer.cs
@@ -402,17 +402,6 @@ public class MtTransformer
         IReadOnlyDictionary<string, string> slotContents
     )
     {
-        // foreach (var slotContentKey in slotContents.Keys)
-        // {
-        //     //Prevent filling a slot that does not exist.
-        //     if (!component.Slots.Contains(slotContentKey))
-        //     {
-        //         var availableSlots = string.Join(", ", component.Slots);
-        //         throw new InvalidSlotException(
-        //             $"A slot with name '{slotContentKey}' couldn't be found in the component, available slots: {availableSlots}.");
-        //     }
-        // }
-        
         foreach (var slotName in component.Slots)
         {
             var slotContent = "";
